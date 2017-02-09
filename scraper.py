a782f6a2ce4d84c26f6707e5ba2080eb377bf10b
Alex Douglas <alex.netley@gmail.com>
2017-02-09 15:38:51 +0000

    Added code for AFS

--- scraper.py
+++ scraper.py
@@ -8,8 +8,8 @@ import sqlite3
 
 def construct_rightmove_url(location, bedrooms, price, is_furnished):
     url = "http://www.rightmove.co.uk/property-to-rent/find.html?locationIdentifier=REGION%{LOCATION}&maxBedrooms="\
-        "{MAX_BEDROOMS}&minBedrooms={MAX_BEDROOMS}&maxPrice={MAX_PRICE}&includeLetAgreed=false&furnishTypes={IS_FURNISHED}"\
-        "&letType=student"
+            "{MAX_BEDROOMS}&minBedrooms={MAX_BEDROOMS}&maxPrice={MAX_PRICE}&includeLetAgreed=false&furnishTypes="\
+            "{IS_FURNISHED}&letType=student"
 
     query_url = url.replace("{LOCATION}", location)  # THIS WILL NEED A HELPER METHOD
 
@@ -25,6 +25,98 @@ def construct_rightmove_url(location, bedrooms, price, is_furnished):
     print(query_url)
 
     get_rightmove_houses(query_url)
+
+
+def construct_afs_url(location, bedrooms, price, is_furnished):
+    url = "http://www.accommodationforstudents.com/searchresults.asp?lookingfor=any&city={LOCATION}&numberofbedrooms="\
+            "{MAX_BEDROOMS}&cost={MAX_PRICE}&searchtype=city&street=&postcode=&area=&orderby=latest&bills_included="\
+            "{IS_FURNISHED}&x=29&y=24&perpage=200"
+
+    query_url = url.replace("{LOCATION}", location)
+
+    query_url = query_url.replace("{MAX_BEDROOMS}", bedrooms)
+
+    monthly_price = int(price)
+
+    weekly_price = int((monthly_price * 12) / 52)
+
+    weekly_price = str(weekly_price)
+
+    query_url = query_url.replace("{MAX_PRICE}", weekly_price)
+
+    if is_furnished is True:
+        query_url = query_url.replace("{IS_FURNISHED}", "1")
+    else:
+        query_url = query_url.replace("{IS_FURNISHED}", "0")
+
+    print(query_url)
+
+    get_afs_houses(query_url, bedrooms)
+
+
+def get_afs_houses(url, bedrooms):
+    result = requests.get(url)
+    page = result.content
+    soup = BeautifulSoup(page, "html.parser")
+
+    link_list = create_afs_links(soup, bedrooms)
+
+    for link in link_list:
+        conn = sqlite3.connect("houses.db")
+        c = conn.cursor()
+
+        c.execute('''SELECT * FROM accommodations WHERE url=?''', (link,))
+
+        result = c.fetchone()
+
+        if result is None:
+
+            data_list = get_soups(link_list)
+
+            for i in range(len(data_list)):
+                house = create_afs_house(data_list[i], link_list[i], bedrooms)
+
+                add_house_to_db(house)
+
+
+def create_afs_house(soup, url, bedrooms):
+    price = soup.find("div", class_="style12x")
+    price = price.text
+    price = re.search('£(.*)pw', price)
+    price = price.group(1)
+    price = int(price)
+    price = int((price * 52) / 12)
+    print(price)
+
+    pattern = re.compile('[A-Z]{1,2}[0-9][0-9A-Z]?\s?[0-9][A-Z]{2}')
+
+    location_string = soup.find(text=pattern)
+    location_string = str(location_string.string)
+    location_string = re.sub(r'\([^)]*\)', '', location_string)
+
+    geolocator = GoogleV3()
+    location = geolocator.geocode(location_string)
+    print(location.latitude)
+    print(location.longitude)
+
+    furnished = soup.find(text=re.compile("Furnished"))
+
+    house = accommodation.Accommodation(price, bedrooms, "UNSURE", 1, url)
+    house.lat = location.latitude
+    house.long = location.longitude
+
+    return house
+
+
+def create_afs_links(soup, bedrooms):
+    link_list = []
+    for item in soup.find_all("span", class_="style13y"):
+        no_of_bedrooms = str(item.contents[0].string)
+        no_of_bedrooms = no_of_bedrooms.rsplit("bed", 1)[0]
+        no_of_bedrooms = no_of_bedrooms.strip()
+        if no_of_bedrooms == bedrooms:
+            link_list.append("http://www.accommodationforstudents.com" + item.parent.attrs["href"])
+    return link_list
 
 
 def get_rightmove_houses(url):
@@ -44,7 +136,7 @@ def get_rightmove_houses(url):
 
         if result is None:
 
-            soup_list = get_rightmove_soups(link_list)
+            soup_list = get_soups(link_list)
 
             for item in soup_list:
 
@@ -87,7 +179,7 @@ def create_rightmove_house(item):
     return house
 
 
-def get_rightmove_soups(link_list):
+def get_soups(link_list):
     soup_list = []
     for link in link_list:
         result = requests.get(link)
@@ -129,5 +221,7 @@ def add_house_to_db(house):
 
 init_db()
 
-construct_rightmove_url("5E219", "4", "400", True)
+construct_afs_url("bristol", "4", "500", False)
+
+construct_rightmove_url("5E219", "4", "500", False)
 
