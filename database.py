05e9677e80d41e09785851f479502875bcc7cedb
Alex Douglas <alex.netley@gmail.com>
2017-03-20 14:19:44 +0000

    Database setup fix

--- database.py
+++ database.py
@@ -8,6 +8,8 @@ def init_db():
     c.execute('''CREATE TABLE IF NOT EXISTS accommodations
                 (url text PRIMARY KEY, ppm int, bedrooms int, bills_inc text, lat real, long real, address text, is_furnished int)''')
 
+    c.execute('''CREATE TABLE IF NOT EXISTS accommodations
+                (city text PRIMARY KEY, outcode text)''')
 
 def add_house_to_db(house):
     conn = sqlite3.connect("houses.db")
